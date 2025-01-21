3b03b4d73968a1ca48bab8995b7d01e481b67a0d
mohamedwael <mohamed.wael@ext.careem.com>
2025-01-21 10:29:52 +0200

    creating handleUIErrorHandler to handle errors on the UI

---
+++ app/src/main/java/com/github/mohamedwael/marvelcharslibrary/characters/presentation/UIErrorHandler.kt
@@ -0,0 +1,28 @@
+package com.github.mohamedwael.marvelcharslibrary.characters.presentation
+
+import com.github.mohamedwael.marvelcharslibrary.R
+import com.github.mohamedwael.marvelcharslibrary.characters.data.MarvelApiException
+import com.github.mohamedwael.marvelcharslibrary.util.ResponseState
+
+fun handleUIErrorHandler(error: Throwable): Int = when (error) {
+    is MarvelApiException -> {
+        when (error) {
+            MarvelApiException.EmptyResponseException -> R.string.error_empty_response
+            MarvelApiException.NoInternetException, MarvelApiException.TimeoutException -> R.string.error_no_internet
+            MarvelApiException.ServerErrorException -> R.string.error_server_unreachable
+            MarvelApiException.UnknownErrorException -> R.string.error_generic
+            MarvelApiException.LimitExceededException -> R.string.error_limit_greater_than_100
+            MarvelApiException.InvalidLimitException -> R.string.error_limit_invalid_or_below_1
+            MarvelApiException.InvalidParameterException -> R.string.error_invalid_or_unrecognized_parameter
+            MarvelApiException.EmptyParameterException -> R.string.error_empty_parameter
+            MarvelApiException.InvalidOrderingException -> R.string.error_invalid_or_unrecognized_ordering_parameter
+            MarvelApiException.TooManyValuesException -> R.string.error_too_many_values
+            MarvelApiException.InvalidFilterValueException -> R.string.error_invalid_filter_value
+        }
+    }
+
+    else -> R.string.error_generic
+}
+
+
+fun ResponseState.Error.getErrorMessageResource() = handleUIErrorHandler(this.exception)
