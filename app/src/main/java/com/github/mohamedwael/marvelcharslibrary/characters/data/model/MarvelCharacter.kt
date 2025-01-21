6bb0ea7de249bab3eedc97fdb192d9c9ee7c62cb
mohamedwael <mohamed.wael@ext.careem.com>
2025-01-21 11:43:29 +0200

    adding required data models

--- app/src/main/java/com/github/mohamedwael/marvelcharslibrary/characters/data/model/MarvelCharacter.kt
+++ app/src/main/java/com/github/mohamedwael/marvelcharslibrary/characters/data/model/MarvelCharacter.kt
@@ -7,6 +7,18 @@ data class MarvelCharacter(
 	@field:SerializedName("thumbnail")
 	val thumbnail: Thumbnail? = null,
 
+	@field:SerializedName("urls")
+	val urls: List<UrlsItem?>? = null,
+
+	@field:SerializedName("stories")
+	val stories: Stories? = null,
+
+	@field:SerializedName("series")
+	val series: Series? = null,
+
+	@field:SerializedName("comics")
+	val comics: Comics? = null,
+
 	@field:SerializedName("name")
 	val name: String? = null,
 
@@ -17,8 +29,11 @@ data class MarvelCharacter(
 	val modified: String? = null,
 
 	@field:SerializedName("id")
-	val id: String? = null,
+	val id: Int? = null,
 
 	@field:SerializedName("resourceURI")
 	val resourceURI: String? = null,
+
+	@field:SerializedName("events")
+	val events: Events? = null
 )
